bd785963a6b8682bc72e68bc5e6fa85d9fe86046
Tim Dohmen <52214801+TimDohmen@users.noreply.github.com>
2020-03-27 13:41:47 -0600

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home container-fluid">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <button type="button" name id class="btn btn-primary btn-lg btn-block">primary</button>
+    <button type="button" name id class="btn btn-secondary btn-lg btn-block">secondary</button>
+    <button type="button" name id class="btn btn-success btn-lg btn-block">success</button>
+    <button type="button" name id class="btn btn-danger btn-lg btn-block">danger</button>
+    <button type="button" name id class="btn btn-warning btn-lg btn-block">warning</button>
+    <button type="button" name id class="btn btn-info btn-lg btn-block">info</button>
+    <button type="button" name id class="btn btn-dark btn-lg btn-block">dark</button>
+    <button type="button" name id class="btn btn-light btn-lg btn-block">light</button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style lang="scss" scoped>
+</style>
